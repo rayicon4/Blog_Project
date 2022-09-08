17619321008bcaa02994c9df918adc17ef5ab09d
rayicon4 <107572914+rayicon4@users.noreply.github.com>
2022-09-08 12:29:57 +0100

    minor updates commit

--- tutorial/urls.py
+++ tutorial/urls.py
@@ -26,6 +26,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('accounts.urls')),
 
-    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
+    url(r'^media/(?P<path>.*)$', serve,{'document_root':  settings.MEDIA_ROOT}), 
     url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
